5c29fb5575694176b5e4353cb2cd1c8993036fca
Alifah63 <55390977+Alifah63@users.noreply.github.com>
2019-09-26 21:18:25 +0800

    Add files via upload

---
+++ exercise1_1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"University :\tUniversiti Teknikal Malaysia Melaka\n";
+    cout<<"Faculty    :\tFTMK\n";
+    cout<<"Programme  :\tBITI\n\n";
+    cout<<"My name is Nik Nor Alifah Ilyana Binti Abd Malik. I am from Selangor.\n";
+    return 0;
+}
